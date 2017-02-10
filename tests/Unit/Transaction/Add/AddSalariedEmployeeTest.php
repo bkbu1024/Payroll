4369929c4a027910f820899003afe0bb9a179cfa
Joo Martin <joomartin@jmweb.hu>
2017-02-10 14:29:17 +0100

    Add Salaried Employee Test Refact
    - Using mock objects

--- tests/Unit/Transaction/Add/AddSalariedEmployeeTest.php
+++ tests/Unit/Transaction/Add/AddSalariedEmployeeTest.php
@@ -5,6 +5,7 @@ namespace Payroll\Tests\Unit\Transaction\Add;
 use Payroll\Factory\Model\Employee as EmployeeFactory;
 use Payroll\PaymentClassification\SalariedClassification;
 use Payroll\PaymentSchedule\MonthlySchedule;
+use Payroll\PaymentSchedule\WeeklySchedule;
 use Payroll\Transaction\Add\AddSalariedEmployee;
 
 class AddSalariedEmployeeTest extends AbstractAddEmployeeTestCase
@@ -13,10 +14,20 @@ class AddSalariedEmployeeTest extends AbstractAddEmployeeTestCase
     {
         $this->data['salary'] = $this->faker->randomFloat(2, 1250, 3750);
 
-        $transaction = new AddSalariedEmployee(
+        $constructorArgs = [
             $this->data['name'],
             $this->data['address'],
-            $this->data['salary']);
+            $this->data['salary']];
+
+        $transaction = $this->getMockObject(AddSalariedEmployee::class, [
+            'getPaymentClassification' => [
+                'return' => new SalariedClassification($this->data['salary']),
+                'times' => 'once'],
+            'getPaymentSchedule' => [
+                'return' => new MonthlySchedule,
+                'times' => 'once'
+            ]
+        ], $constructorArgs);
 
         $this->employee = $transaction->execute();
     }
